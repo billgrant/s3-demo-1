53deee6d42ae0f5f2e0679af184598800304e598
Bill Grant <39649812+billgrant@users.noreply.github.com>
2024-10-22 10:36:28 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 # change the module name if required
 module "s3_pov" {
   # change org-name to your organization name
-  source  = "app.terraform.io/billgrant/s3-pov/aws"
-  version = "0.0.2"
+  source  = "app.terraform.io/billgrant/s3-demo/aws"
+  version = "1.0.2"
   # change tag names if desired
   environment = "dev"
   name        = "test"
@@ -15,24 +15,5 @@ output "bucket_id" {
 
 output "bucket_arn" {
   value       = module.s3_pov.bucket_arn
-  description = "The ARN of the S3 bucket"
-}
-
-module "s3_pov2" {
-  # change org-name to your organization name
-  source  = "app.terraform.io/billgrant/s3-pov/aws"
-  version = "0.0.2"
-  # change tag names if desired
-  environment = "dev2"
-  name        = "test2"
-}
-
-output "bucket_id2" {
-  value       = module.s3_pov2.bucket_id
-  description = "The ID of the S3 bucket"
-}
-
-output "bucket_arn2" {
-  value       = module.s3_pov2.bucket_arn
   description = "The ARN of the S3 bucket"
 }
